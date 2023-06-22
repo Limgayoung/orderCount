b649b9ea443249fa279e112018e268e2b87fabb6
LimGayoung <ygy920@gmail.com>
2023-06-22 17:30:23 +0900

    init

---
+++ src/main/java/nunu/orderCount/OrderCountApplication.java
@@ -0,0 +1,13 @@
+package nunu.orderCount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nunu/orderCount/OrderCountApplicationTests.java
@@ -0,0 +1,13 @@
+package nunu.orderCount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a4815c54748447819e0dd3b8eafe380d934de1b2
LimGayoung <ygy920@gmail.com>
2023-06-26 15:21:44 +0900

    [Fix]#6 - error response 변경

--- src/main/java/nunu/orderCount/global/error/GlobalExceptionHandler.java
+++ src/main/java/nunu/orderCount/global/error/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package nunu.orderCount.global.error;
 
 import nunu.orderCount.global.error.exception.BusinessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,8 +10,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     protected ResponseEntity<ErrorResponse> businessExceptionHandler(BusinessException e){
-        ErrorResponse response = ErrorResponse.of(e.getErrorCode(), e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.valueOf(e.getErrorCode().getStatus()));
+        return ErrorResponse.of(e.getErrorCode(), e.getMessage());
     }
 
     //todo : MethodArgumentNotValidException,BindException, MethodArgumentTypeMismatchException,HttpRequestMethodNotSupportedException

244961fb454288d31cf225c4b39af95f6078a3b3
LimGayoung <ygy920@gmail.com>
2023-12-25 09:15:45 +0900

    [Fix]#55 - order의 주문일자 변수명 변경, 타입 localDateTime으로 변경
    VO와 dto도 localDateTime으로 변경

--- src/main/java/nunu/orderCount/infra/zigzag/model/dto/response/ResponseZigzagOrderDto.java
+++ src/main/java/nunu/orderCount/infra/zigzag/model/dto/response/ResponseZigzagOrderDto.java
@@ -1,8 +1,8 @@
 package nunu.orderCount.infra.zigzag.model.dto.response;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
-import nunu.orderCount.domain.order.model.Order;
 
 @Getter
 public class ResponseZigzagOrderDto {
@@ -12,16 +12,18 @@ public class ResponseZigzagOrderDto {
     private final String orderItemNumber; //상품 주문 번호
     private final String orderNumber; //주문 번호
     private final String productId; //상품 번호
-    private final Long datePaid; //결제 일자
+//    private final Long datePaid; //결제 일자
+    private final LocalDateTime orderDateTime; //결제 일자
 
     @Builder
-    public ResponseZigzagOrderDto(String productName, String option, Long quantity, String orderItemNumber, String orderNumber, String productId, Long datePaid) {
+    public ResponseZigzagOrderDto(String productName, String option, Long quantity, String orderItemNumber, String orderNumber, String productId, LocalDateTime orderDateTime) {
         this.productName = productName;
         this.option = option;
         this.quantity = quantity;
         this.orderItemNumber = orderItemNumber;
         this.orderNumber = orderNumber;
         this.productId = productId;
-        this.datePaid = datePaid;
+        this.orderDateTime = orderDateTime;
+//        this.datePaid = datePaid;
     }
 }
